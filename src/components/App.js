80b69d0780e3c613712517751880e32fb967b3af
josephroyking <joey@bitcoin.com>
2020-01-19 12:49:03 +0100

    Adding faucet link for quick prod deploy

--- src/components/App.js
+++ src/components/App.js
@@ -104,19 +104,32 @@ const App = () => {
                 </Menu.Item>
                 <Menu.SubMenu key="4" title={<span>Links</span>}>
                   <Menu.Item key="5">
-                    <a href="https://exchange.bitcoin.com/" target="_blank">
-                      Exchange
+                    <a href="https://free.bitcoin.com/" target="_blank" rel="noopener noreferrer">
+                      Faucet (Free BCH)
                     </a>
                   </Menu.Item>
                   <Menu.Item key="6">
-                    {" "}
-                    <a href="https://cashgames.bitcoin.com/home" target="_blank">
-                      Games
+                    <a
+                      href="https://exchange.bitcoin.com/"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Exchange
                     </a>
                   </Menu.Item>
                   <Menu.Item key="7">
                     {" "}
-                    <a href="https://local.bitcoin.com" target="_blank">
+                    <a
+                      href="https://cashgames.bitcoin.com/home"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Games
+                    </a>
+                  </Menu.Item>
+                  <Menu.Item key="8">
+                    {" "}
+                    <a href="https://local.bitcoin.com" target="_blank" rel="noopener noreferrer">
                       Trade Locally
                     </a>
                   </Menu.Item>
